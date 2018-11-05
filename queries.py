266d99207c00fd79793635f861e02d671e84bd20
Brian Srebrenik <briansrebrenik@gmail.com>
2018-11-05 16:10:18 -0500

    finished lab

--- queries.py
+++ queries.py
@@ -1,5 +1,5 @@
 from models import *
-from sqlalchemy import create_engine
+from sqlalchemy import *
 
 engine = create_engine('sqlite:///actors.db')
 
@@ -8,7 +8,8 @@ session = Session()
 
 
 def return_gwyneth_paltrows_roles():
-    pass
-
+    gwyneth_paltrow = session.query(Actor).filter(Actor.name == "Gwyneth Paltrow").first()
+    return gwyneth_paltrow.roles
 def return_tom_hanks_2nd_role():
-    pass
+    tom_hanks = session.query(Actor).filter(Actor.name == "Tom Hanks").first()
+    return tom_hanks.roles[1]
--- seed.py
+++ seed.py
@@ -7,3 +7,17 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # add and commit the actors and roles below
+tom_hanks = Actor(name= 'Tom Hanks')
+gwyneth_paltrow = Actor(name= 'Gwyneth Paltrow')
+bill_murray = Actor(name = 'Bill Murray')
+
+tom_hanks.roles = [Role(character = "Forrest Gump"), Role(character = "Jim Lovell"),
+Role(character = "Woody"), Role(character = "Robert Langdon")]
+
+gwyneth_paltrow.roles = [Role(character = "Pepper Potts"),
+Role(character = "Margot Tenenbaum")]
+
+bill_murray.roles = [Role(character = "Phil"), Role(character = "Steve Zissou")]
+
+session.add_all([tom_hanks, gwyneth_paltrow, bill_murray])
+session.commit()
